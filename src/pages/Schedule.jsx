1cc887dfdf2bd4cbb9d876457de0b3aa709af953
Parth@Trivedi <ce21b024@iittp.ac.in>
2025-06-16 11:05:23 +0530

    Image correction

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "./Carousel";
+import { CalendarDays } from "lucide-react";
 
 const Schedule = () => {
   return (
@@ -8,7 +9,47 @@ const Schedule = () => {
       <hr />
       <div className="container py-5">
         <h1 className="text-center mb-4">Schedule</h1>
-        <h5 className="text-center text-danger">To be Updated ...</h5>
+        <div className="container text-center my-4 p-1">
+          <h2 className="text-2xl font-bold text-center mt-2 mb-4">
+            <CalendarDays className="text-primary" size={30} />
+            {"   "}Important Dates <hr className="brW" />
+          </h2>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="dates">
+                <hr className="brMB" />
+                <h4>Workshop Day</h4>
+                <hr className="brW" />
+                <p>
+                  <b>30th November 2025</b>
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="dates">
+                <hr className="brMB" />
+                <h4>Industry Day</h4>
+                <hr className="brW" />
+                <p>
+                  <b>1st Decemebr 2025</b>
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="dates">
+                <hr className="brMB" />
+                <h4>Conference Days</h4>
+                <hr className="brW" />
+                <p>
+                  <b>2nd-3rd December 2025</b>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h5 className="text-center text-danger">
+          Further details to be Updated ...
+        </h5>
       </div>
     </>
   );
